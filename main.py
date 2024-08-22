63dad9a9f31204c9907542ef2d21dac1330798a6
josephkj47 <josephkj47@gmail.com>
2024-08-22 03:40:10 -0400

    data ingestion performed

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from src.cnnClassifier import logger
+import os
 
-logger.info("Welcome to cnn classifier")
+file_path = os.path.join("src", "cnnClassifier", "pipeline", "stage_01_data_ingestion.py")
+print(f"Trying to import from: {file_path}")
+
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
