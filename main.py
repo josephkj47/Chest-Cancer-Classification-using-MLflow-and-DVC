5050635592ea61d8b563eea80ea81ad7f5c9ae67
josephkj47 <josephkj47@gmail.com>
2024-08-23 02:25:49 -0400

    Model training added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.cnnClassifier import logger
 import os
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -14,6 +15,8 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
 STAGE_NAME = "Prepare base model"
 try: 
    logger.info(f"*******************")
@@ -24,3 +27,17 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -9,9 +9,6 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
-from dataclasses import dataclass
-from pathlib import Path
-
 
 @dataclass(frozen=True)
 class PrepareBaseModelConfig:
@@ -23,3 +20,14 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
