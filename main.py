4e1527748e51bf1c7fbc840983f195db43e6f13b
josephkj47 <josephkj47@gmail.com>
2024-08-23 02:01:46 -0400

    Prepared and updated base Model

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from src.cnnClassifier import logger
 import os
-
-file_path = os.path.join("src", "cnnClassifier", "pipeline", "stage_01_data_ingestion.py")
-print(f"Trying to import from: {file_path}")
-
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -16,3 +13,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
