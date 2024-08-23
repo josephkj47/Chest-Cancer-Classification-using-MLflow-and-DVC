289ddf3389148351101559c58ded068c00970044
josephkj47 <josephkj47@gmail.com>
2024-08-23 04:12:02 -0400

    model evaluation with ml flow

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_04_model_evaluation_mlflow import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -41,3 +42,40 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+import json
+import mlflow
+import yaml
+import dagshub
+dagshub.init(repo_owner='josephkj47', repo_name='Chest-Cancer-Classification-using-MLflow-and-DVC', mlflow=True)
+
+# Load parameters from YAML file
+with open('params.yaml', 'r') as f:
+    params = yaml.safe_load(f)
+
+# Load metrics from JSON file
+with open('scores.json', 'r') as f:
+    metrics_data = json.load(f)
+
+# Log parameters to MLflow
+with mlflow.start_run():
+    for key, value in params.items():
+        mlflow.log_param(key, value)
+
+    # Log metrics to MLflow
+    for metric_name, metric_value in metrics_data.items():
+        mlflow.log_metric(metric_name, metric_value)
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from src.cnnClassifier.constants import *
 import os
 from src.cnnClassifier.utils.common import read_yaml, create_directories
-from src.cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig
+from src.cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig, EvaluationConfig
 
 
 class ConfigurationManager:
@@ -70,3 +70,23 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def __init__(
+        self, 
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH):
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        create_directories([self.config.artifacts_root])
+
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/CT-Scan images",
+            mlflow_uri="https://dagshub.com/josephkj47/Chest-Cancer-Classification-using-MLflow-and-DVC.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
